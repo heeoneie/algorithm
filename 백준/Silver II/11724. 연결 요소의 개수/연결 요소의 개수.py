ba0297227f774a978f952a6445816eb2d7d0c548
채희원 <68272931+heeeione@users.noreply.github.com>
2024-02-14 21:21:07 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 752 ms, Memory: 65192 KB -BaekjoonHub

---
+++ 백준/Silver II/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -0,0 +1,25 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
+visited = [False] * (n+1)
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    a,b = map(int, input().split())
+    graph[a] += [b]
+    graph[b] += [a]
+    
+def dfs(graph, v, visited):
+    visited[v] = True
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+cnt = 0
+for i in range(1,n+1):
+    if not visited[i]:
+        dfs(graph, i, visited)
+        cnt += 1
+        
+print(cnt)
