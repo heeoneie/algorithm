2e8725a57620adc3f4bd6ac6885ba5ab9a173a4b
채희원 <68272931+heeeione@users.noreply.github.com>
2024-03-17 17:18:51 +0900

    Update 골드바흐 파티션.py

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
@@ -1,18 +1,18 @@
 import sys
 
 input = sys.stdin.readline
-primeNum = [False, False] + [True]*999999
-for i in range(2, 1000001):
-    if primeNum[i]:
-        for j in range(i*2, 1000001, i):
-            primeNum[j] = False
-            
-T = int(input())
+primeNum = [False, False] + [True] * 999999
+for i in range(2,len(primeNum)):
+    if not primeNum[i]:
+        continue
+    for j in range(i+i,len(primeNum),i):
+        primeNum[j] = False
 
-for i in range(T):
-    count = 0
-    N = int(input())
-    for j in range(2, N//2+1):
-        if primeNum[j] and primeNum[N-j]:
-            count += 1
-    print(count)
+t = int(input().rstrip())
+for _ in range(t):
+    cnt = 0
+    n = int(input().rstrip())
+    for i in range(2,n//2+1):
+        if primeNum[i] and primeNum[n-i]:
+            cnt += 1
+    print(cnt)
