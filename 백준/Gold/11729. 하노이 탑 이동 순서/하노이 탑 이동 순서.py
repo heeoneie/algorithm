a8b994bf486ad3db89c564263ddb97c8cbf93c81
채희원 <68272931+heeeione@users.noreply.github.com>
2024-03-25 14:13:02 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 896 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -0,0 +1,14 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+def hanoi(n,start,end):
+    if n == 1:
+        print(start,end)
+        return
+    hanoi(n-1,start,6-start-end)
+    print(start,end)
+    hanoi(n-1,6-start-end,end)
+    
+n = int(sys.stdin.readline().strip())
+print(2**n-1)
+hanoi(n,1,3)
