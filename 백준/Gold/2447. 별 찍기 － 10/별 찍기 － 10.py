b965cdc734d08d265365e49c0c50a3cce4c3d8d0
채희원 <68272931+heeeione@users.noreply.github.com>
2024-03-25 13:35:26 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 56 ms, Memory: 40544 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -2,19 +2,20 @@ import sys
 sys.setrecursionlimit(10**6)
 
 def drawStar(n):
-    if n == 1:
+    if n==1:
         return ['*']
-
-    stars = drawStar(n//3) 
-    line = []  
-   
+    
+    stars = drawStar(n//3)
+    line = []
+    
     for star in stars:
         line.append(star*3)
     for star in stars:
         line.append(star+' '*(n//3)+star)
     for star in stars:
         line.append(star*3)
+    
     return line
 
-n = int(sys.stdin.readline().strip())
-print('\n'.join(drawStar(n)))
+N = int(sys.stdin.readline())
+print('\n'.join(drawStar(N)))
