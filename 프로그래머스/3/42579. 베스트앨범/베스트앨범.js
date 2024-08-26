95b849ef03e935435bfba09b186585c6e2cf752c
채희원 <68272931+heeeione@users.noreply.github.com>
2024-08-26 16:55:35 +0900

    [level 3] Title: 베스트앨범, Time: 0.53 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,17 +1,20 @@
 function solution(genres, plays) {
     const albumMap = {};
+    
     for (let i=0; i<genres.length; i++) {
-        if (!albumMap[genres[i]]) albumMap[genres[i]] = { totalPlays: 0, songs: [] };
-        albumMap[genres[i]].totalPlays += plays[i];
-        albumMap[genres[i]].songs.push({ id: i, plays: plays[i] });
+        if (!albumMap[genres[i]]) albumMap[genres[i]] = { totalPlay: 0, songs: [] };
+        albumMap[genres[i]].totalPlay += plays[i];
+        albumMap[genres[i]].songs.push({ id: i, play: plays[i]} );
     }
     
-    const sortedGenres = Object.values(albumMap).sort((a,b) => b.totalPlays - a.totalPlays);
+    const sortedGenres = Object.values(albumMap).sort((a,b) => b.totalPlay - a.totalPlay );
     const ans = [];
+    
     for (const genre of sortedGenres) {
-        genre.songs.sort((a,b) => b.plays - a.plays || a.id - b.id);
+        genre.songs.sort((a,b) => b.play - a.play || a.id - b.id);
         ans.push(genre.songs[0].id);
         if (genre.songs.length > 1) ans.push(genre.songs[1].id);
     }
+    
     return ans;
 }
