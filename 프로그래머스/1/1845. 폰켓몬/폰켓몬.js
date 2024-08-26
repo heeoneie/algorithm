65ffa25f6684bc4389167526855beb0f2ba14373
채희원 <68272931+heeeione@users.noreply.github.com>
2024-08-26 16:09:40 +0900

    [level 1] Title: 폰켓몬, Time: 0.59 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,4 +1,4 @@
 function solution(nums) {
-    const poketmons = new Set(nums);
-    return Math.min(poketmons.size, nums.length/2);
+    const poketmonMap = new Set(nums);
+    return Math.min(poketmonMap.size, nums.length/2);
 }
