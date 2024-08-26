a59aa7c98267d253ed1d30da906e731b3243712d
채희원 <68272931+heeeione@users.noreply.github.com>
2024-08-26 16:27:03 +0900

    [level 2] Title: 전화번호 목록, Time: 132.53 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,7 +1,6 @@
 function solution(phone_book) {
     phone_book.sort();
-    for (let i=0; i<phone_book.length-1; i++) {
+    for (let i=0; i<phone_book.length-1; i++) 
         if (phone_book[i+1].startsWith(phone_book[i])) return false;
-    }
     return true;
 }
