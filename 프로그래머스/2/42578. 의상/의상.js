9fb7319904f23d4e1c8e72b8079db429235b6e22
채희원 <68272931+heeeione@users.noreply.github.com>
2024-08-26 16:39:32 +0900

    [level 2] Title: 의상, Time: 0.38 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,13 +1,11 @@
 function solution(clothes) {
     const clothesMap = {};
-    
     for (const [name, type] of clothes) {
-        if (clothesMap[type]) clothesMap[type]++;
-        else clothesMap[type] = 1;
+        if (!clothesMap[type]) clothesMap[type] = 1;
+        else clothesMap[type]++;
     }
-    
     let ans = 1;
     for (const key in clothesMap) ans *= (clothesMap[key]+1);
     
-    return ans-1;
+    return ans-1; 
 }
