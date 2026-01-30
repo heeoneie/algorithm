c9571caa8b17897c5e57de73b82addd249481b42
채희원 <68272931+heeoneie@users.noreply.github.com>
2026-01-31 02:21:33 +0900

    [level 2] Title: 올바른 괄호, Time: 8.70 ms, Memory: 9.54 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,21 +1,13 @@
-'''
-s의 0번째가 (인지 체크 아니면 false
-열린 괄호면 append하고 닫힌 괄호면 열린 괄호 있는 지 체크해서 없으면 false
-있으면 열린 괄호 pop
-stack에 원소가 남아 있다면 false
-'''
 def solution(s):
-    answer = True
-    stack = []
-    
-    for i in s:
-        if i == '(':
-            stack.append(i)
-        else: 
-            if stack == []:
-                return False
-            else:
-                stack.pop()
-    if len(stack) > 0:
+    if s[0] == ')':
         return False
-    return True
+    
+    temp = []
+    for char in s:
+        if char == '(':
+            temp.append(char)
+        else:
+            if len(temp) == 0:
+                return False
+            temp.pop()
+    return len(temp) == 0
