88e9262042088a823218879dec73e6278ec58520
채희원 <68272931+heeoneie@users.noreply.github.com>
2026-01-31 18:04:42 +0900

    [level 2] Title: 기능개발, Time: 0.42 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=python3) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.47 ms
+메모리: 9.13 MB, 시간: 0.42 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 04일 22:26:08
+2026년 01월 31일 18:04:37
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,22 +1,17 @@
-'''
-0번째 기능이 100찍을때까지 time ++
-100이면 pop시키고 cnt ++
-'''
 def solution(progresses, speeds):
     answer = []
-    time = 0
-    cnt = 0
-    
-    while len(speeds) > 0:
-        if progresses[0] + time*speeds[0] >= 100:
+    # 앞선 기능이 다 개발되어야 배포 가능
+    # 로직 = 루프 돌면서 인덱스에 맞는 스피드 +
+    # 앞에 기능이 100보다 크거나 같으면 앞에꺼 pop하고 또 그 다음 기능이 100보다 크거나 같은지 확인
+    while len(progresses) > 0:
+        for i in range(len(progresses)):
+            progresses[i] += speeds[i]
+        cnt = 0
+            # 그 다음 기능도 100인지 봐야하는데 그럼 계속 if문을 추가하는거니까 일반화하는 방법이 있을 것 같은데
+        while len(progresses) > 0 and progresses[0] >= 100:
             progresses.pop(0)
             speeds.pop(0)
             cnt += 1
-            
-        else:
-            if cnt > 0:
-                answer.append(cnt)
-                cnt = 0
-            time += 1
-    answer.append(cnt)
+        if cnt > 0:
+            answer.append(cnt)
     return answer
